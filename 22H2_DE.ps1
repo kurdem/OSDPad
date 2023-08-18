3b6a39423f72ac23017c1de65f0ba8b44ad02303
Meik Kurde <43165848+kurdem@users.noreply.github.com>
2023-08-18 15:32:32 +0200

    Update 22H2_DE.ps1

--- 22H2_DE.ps1
+++ 22H2_DE.ps1
@@ -11,3 +11,20 @@ $Params = @{
     SkipODT = $true
 }
 Start-OSDCloud @Params
+
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
+powershell.exe -Command "& {IEX (IRM oobetasks.osdcloud.ch)}"
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
